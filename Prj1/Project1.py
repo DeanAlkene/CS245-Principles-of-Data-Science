07753c5f5e8b4b986287fb8dad906b8cc1596b7d
DeanAlkene <deanlhz@sjtu.edu.cn>
2020-03-31 21:26:49 +0800

    Start Prj1

--- Prj1/Project1.py
+++ Prj1/Project1.py
@@ -7,8 +7,8 @@ from sklearn.svm import SVC
 x_file_name = "Animals_with_Attributes2/Features/ResNet101/AwA2-features.txt"
 y_file_name = "Animals_with_Attributes2/Features/ResNet101/AwA2-labels.txt"
 col_name = ['feature' + str(i) for i in range(2048)]
-x_data = pd.read_csv(x_file_name, sep=' ', nrows=2000, names=col_name)
-y_data = pd.read_csv(y_file_name, nrows=2000, names=['label'])
+x_data = pd.read_csv(x_file_name, sep=' ', names=col_name)
+y_data = pd.read_csv(y_file_name, names=['label'])
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.4, random_state=101)
 x_train = x_train.values
 y_train = y_train.values.ravel()
