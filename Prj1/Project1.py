b1e540a084cc94c84f29190d6e50943b4c815810
DeanAlkene <deanlhz@sjtu.edu.cn>
2020-03-31 17:44:54 +0800

    Start Prj1

--- Prj1/Project1.py
+++ Prj1/Project1.py
@@ -1,6 +1,22 @@
 import numpy as np
 import pandas as pd
 import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
-file_name = "Animals_with_Attributes2/Features/ResNet101/AwA2-features.txt"
-data = pd.read_csv(file_name, sep=' ', nrows=100)
+x_file_name = "Animals_with_Attributes2/Features/ResNet101/AwA2-features.txt"
+y_file_name = "Animals_with_Attributes2/Features/ResNet101/AwA2-labels.txt"
+col_name = ['feature' + str(i) for i in range(2048)]
+x_data = pd.read_csv(x_file_name, sep=' ', nrows=2000, names=col_name)
+y_data = pd.read_csv(y_file_name, nrows=2000, names=['label'])
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.4, random_state=101)
+x_train = x_train.values
+y_train = y_train.values.ravel()
+x_test = x_test.values
+y_test = y_test.values
+model = SVC(C=1.0, kernel='rbf', gamma='auto', verbose=True)
+model.fit(x_train, y_train)
+result = model.predict(x_test)
+tmp = [int(result[i] == y_test[i]) for i in range(len(result))]
+acc = sum(tmp) / len(tmp)
+print("Accuracy: %f"%acc)
