c98915b0e1244dab442a2e8d10171ebd8e154407
DeanAlkene <deanlhz@sjtu.edu.cn>
2020-04-02 16:55:05 +0800

    Start Prj1

--- Prj1/Project1.py
+++ Prj1/Project1.py
@@ -11,12 +11,10 @@ x_data = pd.read_csv(x_file_name, sep=' ', names=col_name)
 y_data = pd.read_csv(y_file_name, names=['label'])
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.4, random_state=101)
 x_train = x_train.values
-y_train = y_train.values.ravel()
+y_train = y_train.values
 x_test = x_test.values
 y_test = y_test.values
 model = SVC(C=1.0, kernel='rbf', gamma='auto', verbose=True)
 model.fit(x_train, y_train)
-result = model.predict(x_test)
-tmp = [int(result[i] == y_test[i]) for i in range(len(result))]
-acc = sum(tmp) / len(tmp)
+acc = model.score(x_test, y_test)
 print("Accuracy: %f"%acc)
