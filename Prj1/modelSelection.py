cbd376041b52e785d273a933eb8165da8f95c0de
DeanAlkene <40763618+DeanAlkene@users.noreply.github.com>
2020-04-07 13:39:02 +0800

    Model Selection Coding

--- Prj1/modelSelection.py
+++ Prj1/modelSelection.py
@@ -13,32 +13,62 @@ col_name = ['feature' + str(i) for i in range(2048)]
 X_data = pd.read_csv(X_file_name, sep=' ', names=col_name)
 y_data = pd.read_csv(y_file_name, names=['label'])
 
-def fineTuning(X, y, k=5):
-    Cs = [0.1 * i for i in range(1, 21)]
-    cv_scores = []
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
-    for c in Cs:
-        print("\nCross-Validation: C=%0.2f\n"%(c))
+def coarseTuning(X, y, k=5):
+    C_range = np.logspace(-5, 5, 11)
+    cv_scores_rbf = []
+    cv_scores_linear = []
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+
+    for c in C_range:
+        print("\nCross-Validation rbf: C=%0.2f\n"%(c))
         model = SVC(C=c, kernel='rbf', gamma='auto', verbose=False)
         score = cross_val_score(model, X_train, y_train, cv=k, scoring='accuracy')
-        cv_scores.append(score.mean())
-    bestC = Cs[cv_scores.index(max(cv_scores))]
-    plt.plot(Cs, cv_scores, 'bo-', linewidth=2)
-    plt.title('Parameter Tuning')
+        cv_scores_rbf.append(score.mean())
+    bestC_rbf = C_range[cv_scores_rbf.index(max(cv_scores_rbf))]
+    plt.plot(C_range, cv_scores_rbf, 'bo-', linewidth=2)
+    plt.title('SVM with rbf kernel')
     plt.xlabel('C')
-    plt.xticks([0.1 * i for i in range(0, 22)])
+    plt.xticks(np.logspace(-6, 6, 13))
     plt.ylabel('Accuracy')
-    plt.savefig('TuningParam.jpg')
+    plt.savefig('CoarseTuningParam_rbf.jpg')
 
-    bestModel = SVC(C=bestC, kernel='rbf', gamma='auto', verbose=False)
-    bestModel.fit(X_train, y_train)
-    bestAcc = bestModel.score(X_test, y_test)
-    with open('res.txt', 'w') as f:
-        f.write("The best C = %0.2f\n"%(bestC))
-        f.write("Acc = %f"%(bestAcc))
+    for c in C_range:
+        print("\nCross-Validation linear: C=%0.2f\n"%(c))
+        model = SVC(C=c, kernel='linear', gamma='auto', verbose=False)
+        score = cross_val_score(model, X_train, y_train, cv=k, scoring='accuracy')
+        cv_scores_linear.append(score.mean())
+    bestC_linear = C_range[cv_scores_linear.index(max(cv_scores_linear))]
+    plt.plot(C_range, cv_scores_linear, 'bo-', linewidth=2)
+    plt.title('SVM with linear kernel')
+    plt.xlabel('C')
+    plt.xticks(np.logspace(-6, 6, 13))
+    plt.ylabel('Accuracy')
+    plt.savefig('CoarseTuningParam_linear.jpg')
+
+    bestModel_rbf = SVC(C=bestC_rbf, kernel='rbf', gamma='auto', verbose=False)
+    bestModel_rbf.fit(X_train, y_train)
+    bestAcc_rbf = bestModel_rbf.score(X_test, y_test)
+
+    bestModel_linear = SVC(C=bestC_linear, kernel='linear', gamma='auto', verbose=False)
+    bestModel_linear.fit(X_train, y_train)
+    bestAcc_linear = bestModel_linear.score(X_test, y_test)
+
+    with open('res_coarse.txt', 'w') as f:
+        f.write("rbf:\n")
+        for i in range(len(C_range)):
+            f.write("rbf: C = %f, acc = %0.2f\n"%(C_range[i], cv_scores_rbf[i]))
+        f.write("rbf: Best C = %0.2f\n"%(bestC_rbf))
+        f.write("rbf: acc = %f\n"%(bestAcc_rbf))
+        for i in range(len(C_range)):
+            f.write("linear: C = %f, acc = %0.2f\n"%(C_range[i], cv_scores_linear[i]))
+        f.write("linear: Best C = %0.2f\n"%(bestC_linear))
+        f.write("linear: acc = %f\n"%(bestAcc_linear))
+
+def fineTuning(X, y, k=5):
+    pass
 
 def main():
-    fineTuning(X_data, y_data)
+    coarseTuning(X_data, y_data)
 
 if __name__ == '__main__':
     main()
