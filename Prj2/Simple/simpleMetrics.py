79edf883b9039a5d4376c3c9191b7d4b8765cb6f
DeanAlkene <deanlhz@sjtu.edu.cn>
2020-04-29 20:39:48 +0800

    Start Prj2

---
+++ Prj2/Simple/simpleMetrics.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.neighbors import KNeighborsClassifier, DistanceMetric
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics.pairwise import cosine_distances
+import sys
+sys.path.append("..")
+from processData import loadDataDivided
+import KNN
+
+def main():
+    kernel_range = ['linear', 'rbf', 'poly', 'sigmoid', 'cosine']
+    dim_range = [50, 500, 2048]
+    k_range = [2, 3, 4, 5, 6, 7, 8, 10, 15]
+    metric_range = ['euclidean', 'manhattan', 'chebyshev']
+    for dim in dim_range:
+        for kernel in kernel_range:
+            if dim != 2048:
+                X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=True, suffix='_' + str(dim) + '_' + kernel)
+            else:
+                X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=True, suffix='')
+            for metric in metric_range:
+                KNN.runKNN(X_train, X_test, y_train, y_test, k_range, metric=metric, metric_params=None, label=str(dim) + '_' + kernel + '_' + metric)
+    
+    for dim in dim_range:
+        for kernel in kernel_range:
+            if dim != 2048:
+                X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=True, suffix='_' + str(dim) + '_' + kernel)
+            else:
+                X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=True, suffix='')
+            KNN.runKNN(X_train, X_test, y_train, y_test, k_range, metric=cosine_distances, metric_params=None, label=str(dim) + '_' + kernel + '_cosine')
