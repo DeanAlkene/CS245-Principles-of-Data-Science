6980b77d43673c5789ef88ea0a09c7e68cba30ac
DeanAlkene <deanlhz@sjtu.edu.cn>
2020-05-06 20:19:36 +0800

    Using cosine distance

--- Prj2/Simple/simpleMetrics.py
+++ Prj2/Simple/simpleMetrics.py
@@ -5,38 +5,45 @@ import sklearn
 from sklearn.neighbors import KNeighborsClassifier, DistanceMetric
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics.pairwise import cosine_distances
 import sys
 sys.path.append("..")
 from processData import loadDataDivided
 import KNN
+
+def cosine(x, y):
+    	s = np.linalg.norm(x, ord=2) * np.linalg.norm(y, ord=2)
+	if s == 0:
+		return 0
+	return 1 - np.dot(x, y) / s
 
 def main():
     kernel_range = ['linear', 'rbf', 'poly', 'sigmoid', 'cosine']
     dim_range = [50, 500, 2048]
     k_range = [2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 50, 100, 200, 500, 1000]
     metric_range = ['euclidean', 'manhattan', 'chebyshev']
-    for dim in dim_range:
-        for metric in metric_range:
-            if dim != 2048:
-                for kernel in kernel_range:
-                    print("dim: %d, kernel: %s, metric: %s" % (dim, kernel, metric))
-                    X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=False, suffix='_' + str(dim) + '_' + kernel)
-                    KNN.runKNN(X_train, X_test, y_train, y_test, k_range, metric=metric, metric_params=None, label=str(dim) + '_' + kernel + '_' + metric)
-            else:
-                X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=False, suffix='')
-                print("dim: %d, metric: %s" % (dim, metric))
-                KNN.runKNN(X_train, X_test, y_train, y_test, k_range, metric=metric, metric_params=None, label=str(dim) + '_' + metric)
+    # for dim in dim_range:
+    #     for metric in metric_range:
+    #         if dim != 2048:
+    #             for kernel in kernel_range:
+    #                 print("dim: %d, kernel: %s, metric: %s" % (dim, kernel, metric))
+    #                 X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=False, suffix='_' + str(dim) + '_' + kernel)
+    #                 KNN.runKNN(X_train, X_test, y_train, y_test, k_range, metric=metric, metric_params=None, label=str(dim) + '_' + kernel + '_' + metric)
+    #         else:
+    #             X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=False, suffix='')
+    #             print("dim: %d, metric: %s" % (dim, metric))
+    #             KNN.runKNN(X_train, X_test, y_train, y_test, k_range, metric=metric, metric_params=None, label=str(dim) + '_' + metric)
             
     
-#    for dim in dim_range:
-#        for kernel in kernel_range:
-#            if dim != 2048:
-#                X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=True, suffix='_' + str(dim) + '_' + kernel)
-#            else:
-#                X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=True, suffix='')
-#            print("dim: %d, kernel: %s, metric: %s" % (dim, kernel, "cosine"))
-#            KNN.runKNN(X_train, X_test, y_train, y_test, k_range, metric=cosine_distances, metric_params=None, label=str(dim) + '_' + kernel + '_cosine')
+    for dim in dim_range:
+        if dim != 2048:
+            for kernel in kernel_range:
+                print("dim: %d, kernel: %s, metric: %s" % (dim, kernel, "cosine"))
+                X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=False, suffix='_' + str(dim) + '_' + kernel)
+                KNN.runKNN(X_train, X_test, y_train, y_test, k_range, metric=metric, metric_params=None, label=str(dim) + '_' + kernel + '_cosine')
+        else:
+            X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=False, suffix='')
+            print("dim: %d, metric: %s" % (dim, "cosine"))
+            KNN.runKNN(X_train, X_test, y_train, y_test, k_range, metric=metric, metric_params=None, label=str(dim) + '_cosine')
 
 if __name__ == '__main__':
     main()
