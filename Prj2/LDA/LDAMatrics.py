e6d226bdd21270390a17bdb3b50f601bcae77dc6
KylinC <k1017856853@icloud.com>
2020-05-13 13:20:34 +0800

    LDA cloesd

---
+++ Prj2/LDA/LDAMatrics.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.neighbors import KNeighborsClassifier, DistanceMetric
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+import sys
+
+sys.path.append("..")
+from processData import loadDataDivided
+import KNN
+
+
+def cosine(x, y):
+    s = np.linalg.norm(x, ord=2) * np.linalg.norm(y, ord=2)
+    if s == 0:
+        return 0
+    return 1 - np.dot(x, y) / s
+
+
+def main():
+    kernel= 'LDA'
+    dim_range = [2, 3, 5, 10, 20, 30, 40]
+    k_range = [2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 50, 100, 200, 500, 1000]
+    metric_range = ['euclidean', 'manhattan', 'chebyshev']
+
+    for dim in dim_range:
+        for metric in metric_range:
+            print("dim: %d, kernel: %s, metric: %s" % (dim, kernel, metric))
+            X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=False, suffix='_' + str(dim) + '_' + kernel)
+            KNN.runKNN(X_train, X_test, y_train, y_test, k_range, metric=metric, metric_params=None, label=str(dim) + '_' + kernel + '_' + metric)
+
+    # for dim in dim_range:
+    #     print("dim: %d, kernel: %s, metric: %s" % (dim, kernel, "cosine"))
+    #     X_train, X_test, y_train, y_test = loadDataDivided(ifSubDir=True, ifScale=False,
+    #                                                        suffix='_' + str(dim) + '_' + kernel)
+    #     KNN.runKNN(X_train, X_test, y_train, y_test, k_range, metric=cosine, metric_params=None,
+    #                label=str(dim) + '_' + kernel + '_cosine')
+
+
+if __name__ == '__main__':
+    main()
