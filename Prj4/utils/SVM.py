30fce0be55c60bc757fedc21c2564786ffdebaed
DeanAlkene <deanlhz@sjtu.edu.cn>
2020-06-14 00:23:14 +0800

    Deadlock occurs with sklearn GridSearchCV using multiple threads, what a garbage package

--- Prj4/utils/SVM.py
+++ Prj4/utils/SVM.py
@@ -10,7 +10,7 @@ def SVM(X_train, X_test, y_train, y_test):
     X_test = scaler.fit_transform(X_test)
     params = {'kernel': ('linear', 'rbf'), 'C': [0.001, 0.01, 0.1, 1, 10]}
     svc = SVC()
-    clf = GridSearchCV(estimator=svc, param_grid=params, verbose=True, n_jobs=-1)
+    clf = GridSearchCV(estimator=svc, param_grid=params, verbose=True, n_jobs=8)
     clf.fit(X_train, y_train)
     score = clf.best_estimator_.score(X_test, y_test)
     print("score: %f" % (score))
