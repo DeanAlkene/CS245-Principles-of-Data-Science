03d4a7989d64f728c31b8efea53a448e8378552e
DeanAlkene <deanlhz@sjtu.edu.cn>
2020-06-17 23:33:44 +0800

    EasyTL running done

---
+++ Prj4/Traditional/BDA.py
@@ -0,0 +1,196 @@
+# encoding=utf-8
+import sys
+sys.path.append('..')
+from utils import dataloader, SVM, TSNE
+import numpy as np
+import scipy.io
+import scipy.linalg
+import sklearn.neighbors
+from sklearn import metrics
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+import multiprocessing
+
+params_dict = {'ARC': 10.0, 'ARK': 'rbf', 'CRC': 0.01, 'CRK': 'linear', 'PRC': 10.0, 'PRK': 'rbf'}
+
+def kernel(ker, X1, X2, gamma):
+    K = None
+    if not ker or ker == 'primal':
+        K = X1
+    elif ker == 'linear':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.linear_kernel(
+                np.asarray(X1).T, np.asarray(X2).T)
+        else:
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T)
+    elif ker == 'rbf':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.rbf_kernel(
+                np.asarray(X1).T, np.asarray(X2).T, gamma)
+        else:
+            K = sklearn.metrics.pairwise.rbf_kernel(
+                np.asarray(X1).T, None, gamma)
+    return K
+
+
+def proxy_a_distance(source_X, target_X):
+    """
+    Compute the Proxy-A-Distance of a source/target representation
+    """
+    nb_source = np.shape(source_X)[0]
+    nb_target = np.shape(target_X)[0]
+
+    train_X = np.vstack((source_X, target_X))
+    train_Y = np.hstack((np.zeros(nb_source, dtype=int),
+                         np.ones(nb_target, dtype=int)))
+
+    clf = svm.LinearSVC(random_state=0)
+    clf.fit(train_X, train_Y)
+    y_pred = clf.predict(train_X)
+    error = metrics.mean_absolute_error(train_Y, y_pred)
+    dist = 2 * (1 - 2 * error)
+    return dist
+
+
+def estimate_mu(_X1, _Y1, _X2, _Y2):
+    adist_m = proxy_a_distance(_X1, _X2)
+    C = len(np.unique(_Y1))
+    epsilon = 1e-3
+    list_adist_c = []
+    for i in range(1, C + 1):
+        ind_i, ind_j = np.where(_Y1 == i), np.where(_Y2 == i)
+        Xsi = _X1[ind_i[0], :]
+        Xtj = _X2[ind_j[0], :]
+        adist_i = proxy_a_distance(Xsi, Xtj)
+        list_adist_c.append(adist_i)
+    adist_c = sum(list_adist_c) / C
+    mu = adist_c / (adist_c + adist_m)
+    if mu > 1:
+        mu = 1
+    if mu < epsilon:
+        mu = 0
+    return mu
+
+
+class BDA:
+    def __init__(self, kernel_type='primal', dim=30, lamb=1, mu=0.5, gamma=1, T=10, mode='BDA', estimate_mu=False):
+        '''
+        Init func
+        :param kernel_type: kernel, values: 'primal' | 'linear' | 'rbf'
+        :param dim: dimension after transfer
+        :param lamb: lambda value in equation
+        :param mu: mu. Default is -1, if not specificied, it calculates using A-distance
+        :param gamma: kernel bandwidth for rbf kernel
+        :param T: iteration number
+        :param mode: 'BDA' | 'WBDA'
+        :param estimate_mu: True | False, if you want to automatically estimate mu instead of manally set it
+        '''
+        self.kernel_type = kernel_type
+        self.dim = dim
+        self.lamb = lamb
+        self.mu = mu
+        self.gamma = gamma
+        self.T = T
+        self.mode = mode
+        self.estimate_mu = estimate_mu
+
+    def fit_predict(self, Xs, Ys, Xt, Yt, tag):
+        '''
+        Transform and Predict using 1NN as JDA paper did
+        :param Xs: ns * n_feature, source feature
+        :param Ys: ns * 1, source label
+        :param Xt: nt * n_feature, target feature
+        :param Yt: nt * 1, target label
+        :return: acc, y_pred, list_acc
+        '''
+        list_acc = []
+        X = np.hstack((Xs.T, Xt.T))
+        X /= np.linalg.norm(X, axis=0)
+        m, n = X.shape
+        ns, nt = len(Xs), len(Xt)
+        e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
+        C = len(np.unique(Ys))
+        H = np.eye(n) - 1 / n * np.ones((n, n))
+        mu = self.mu
+        M = 0
+        Y_tar_pseudo = None
+        Xs_new = None
+        for t in range(self.T):
+            N = 0
+            M0 = e * e.T * C
+            if Y_tar_pseudo is not None and len(Y_tar_pseudo) == nt:
+                for c in range(1, C + 1):
+                    e = np.zeros((n, 1))
+                    Ns = len(Ys[np.where(Ys == c)])
+                    Nt = len(Y_tar_pseudo[np.where(Y_tar_pseudo == c)])
+
+                    if self.mode == 'WBDA':
+                        Ps = Ns / len(Ys)
+                        Pt = Nt / len(Y_tar_pseudo)
+                        alpha = Pt / Ps
+                        mu = 1
+                    else:
+                        alpha = 1
+
+                    tt = Ys == c
+                    e[np.where(tt == True)] = 1 / Ns
+                    yy = Y_tar_pseudo == c
+                    ind = np.where(yy == True)
+                    inds = [item + ns for item in ind]
+                    e[tuple(inds)] = -alpha / Nt
+                    e[np.isinf(e)] = 0
+                    N = N + np.dot(e, e.T)
+
+            # In BDA, mu can be set or automatically estimated using A-distance
+            # In WBDA, we find that setting mu=1 is enough
+            if self.estimate_mu and self.mode == 'BDA':
+                if Xs_new is not None:
+                    mu = estimate_mu(Xs_new, Ys, Xt_new, Y_tar_pseudo)
+                else:
+                    mu = 0
+            M = (1 - mu) * M0 + mu * N
+            M /= np.linalg.norm(M, 'fro')
+            K = kernel(self.kernel_type, X, None, gamma=self.gamma)
+            n_eye = m if self.kernel_type == 'primal' else n
+            a, b = np.linalg.multi_dot(
+                [K, M, K.T]) + self.lamb * np.eye(n_eye), np.linalg.multi_dot([K, H, K.T])
+            w, V = scipy.linalg.eig(a, b)
+            ind = np.argsort(w)
+            A = V[:, ind[:self.dim]]
+            Z = np.dot(A.T, K)
+            Z /= np.linalg.norm(Z, axis=0)
+            Xs_new, Xt_new = Z[:, :ns].T, Z[:, ns:].T
+
+            clf = svm.SVC(C=params_dict[tag + 'C'], kernel=params_dict[tag + 'K'])
+            clf.fit(Xs_new, Ys.ravel())
+            Y_tar_pseudo = clf.predict(Xt_new)
+            acc = sklearn.metrics.accuracy_score(Yt, Y_tar_pseudo)
+            list_acc.append(acc)
+            print('{} iteration [{}/{}]: Acc: {:.4f}'.format(self.mode, t + 1, self.T, acc))
+        return acc, Xs_new, Xt_new
+
+
+def runBDA():
+    pairs = [('Art', 'RealWorld'), ('Clipart', 'RealWorld'), ('Product', 'RealWorld')]
+    kernels = ['primal', 'linear', 'rbf']
+    dim_range = [32, 64, 128, 256, 512, 1024, 2048]
+    for p in pairs:
+        print("%s->%s" % (p[0], p[1]))
+        X_train, y_train, X_test, y_test = dataloader.loadData(p[0], p[1])
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.fit_transform(X_test)
+        for k in kernels:
+            for n in dim_range:
+                print("kernel: %s, dim: %d" % (k, n))
+                model = BDA(kernel_type=k, dim=n, mode='WBDA')
+                score, X_train_new, X_test_new = model.fit_predict(X_train, y_train, X_test, y_test, p[0][0] + p[1][0])
+                TSNE.draw(X_train_new, X_test_new, p[0][0] + '_' + p[1][0] + '_BDA_' + k + '_' + str(n), p[0] + '->' + p[1])
+                with open('BDA.txt', 'a') as f:
+                    f.write('%s->%s, kernel=%s, dim=%d, with acc=%f\n' % (p[0], p[1], k, n, score))
+        print()
+        
+if __name__ == '__main__':
+    multiprocessing.set_start_method('forkserver')
+    runBDA()
